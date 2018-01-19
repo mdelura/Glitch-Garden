ef63cf3153d3d2d4c38370eb826eac7cd5fecc88
Michal Delura <deluramichal@gmail.com>
2018-01-19 22:58:26 +0100

    Change MusicPlayer into MusicManager and add main scenes

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,5 +7,8 @@ public class LevelManager : MonoBehaviour
 
     public void LoadNextLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
+    public void LoadPreviousLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
     public void QuitRequest() => Application.Quit();
+
 }
---
+++ Assets/Scripts/PersistentMusicManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentMusicManager : MonoBehaviour
+{
+    public AudioClip[] sceneClips;
+
+    public float splashScreenDuration = 3;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += SceneLoaded;
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void LoadStartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    private void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex  > sceneClips.Length - 1 || !sceneClips[scene.buildIndex])
+        {
+            return;
+        }
+
+        _audioSource.Stop();
+        _audioSource.clip = sceneClips[scene.buildIndex];
+
+        if (scene.buildIndex == 0)
+        {
+            Invoke(nameof(LoadStartScene), splashScreenDuration);
+        }
+        else
+        {
+            _audioSource.loop = true;
+        }
+
+        _audioSource.Play();
+    }
+}

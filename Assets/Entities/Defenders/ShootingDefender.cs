161ed43f4a778dd9dbe550cb61eff3b77bde893c
Michal Delura <deluramichal@gmail.com>
2018-02-02 19:06:40 +0100

    Add defender selection buttons, refactor Projectiles in-scene management.

--- Assets/Entities/Defenders/ShootingDefender.cs
+++ Assets/Entities/Defenders/ShootingDefender.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,25 @@ public class ShootingDefender : Defender
 
     public GameObject projectile;
 
+    private GameObject _projectileParent;
+
+    private void Start()
+    {
+        _projectileParent = FindOrSpawn("Projectiles");
+    }
+
+    private GameObject FindOrSpawn(string name)
+    {
+        GameObject gameObject = GameObject.Find(name);
+        if (gameObject)
+            return gameObject;
+        return new GameObject(name);
+    }
+
     private void Shoot()
     {
         var newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity);
-        newProjectile.transform.parent = GameObject.Find("Projectiles").transform;
+        newProjectile.transform.parent = _projectileParent.transform;
 
     }
 }
--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -7,19 +7,23 @@ public class Button : MonoBehaviour
 {
     private GameObject _buttons;
 
+    public GameObject associatedDefender;
+
+    public static GameObject SelectedDefender { get; private set; }
+
     private void Start()
     {
         _buttons = GameObject.Find("Buttons");
     }
 
-
     private void OnMouseDown()
     {
         Debug.Log($"Button {name} clicked.");
 
+        SelectedDefender = associatedDefender;
+
         foreach (var spriteRenderer in _buttons.GetComponentsInChildren<SpriteRenderer>())
         {
-            
             float alpha = spriteRenderer.gameObject == gameObject ? 1 : 0.3f;
             spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, alpha);
         } 

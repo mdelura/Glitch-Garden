b75f1c1c1b8890d87bebc37f9641a925b8745956
Michal Delura <deluramichal@gmail.com>
2018-02-03 18:28:23 +0100

    Detect Attackers

--- Assets/Entities/Defenders/ShootingDefender.cs
+++ Assets/Entities/Defenders/ShootingDefender.cs
@@ -1,20 +1,45 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 
 public class ShootingDefender : Defender
 {
+    const float startShootingXPos = 9;
+
     public GameObject gun;
     public GameObject projectile;
 
     private GameObject _projectileParent;
 
+    private GameObject _attackerSpawner;
+    private int _lane;
+
+
+    private Animator _animator;
+
     private void Start()
     {
         _projectileParent = Util.FindOrSpawn("Projectiles");
+        _animator = GetComponent<Animator>();
+
+        _attackerSpawner = GameObject.Find("AttackerSpawner");
+        _lane = GetLane(transform.position);
     }
 
+    private int GetLane(Vector3 position) => Mathf.RoundToInt(position.y);
+
+    private void Update()
+    {
+        _animator.SetBool(AnimatorParam.IsAttacking, IsAttackerAheadInLane());
+    }
+
+    private bool IsAttackerAheadInLane()
+    {
+        return _attackerSpawner.transform
+            .Cast<Transform>()
+            .Any(t => GetLane(t.position) == _lane &&
+                t.position.x > transform.position.x &&
+                t.position.x <= startShootingXPos);
+    }
 
     private void Shoot()
     {
